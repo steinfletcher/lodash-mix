6fbbc03ffdf3c223c2ad4f03088498f4fa894354
Stein <steinfletcher@gmail.com>
2014-08-14 15:07:47 +0100

    Add test runner bash script

--- test-runner.bash
+++ test-runner.bash
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+# Run tests in node and browser.
+# dependencies:
+#      browserify and tape should be installed globally
+
+BROWSER_CMD=firefox
+
+# node tests
 tape test/spec/*.spec.js | faucet
+
+# browser tests
+mkdir -p .tmp/
+browserify test/spec/*.spec.js > .tmp/bundle.js
+echo '<script src="bundle.js"></script>' > .tmp/test.html
+${BROWSER_CMD} .tmp/test.html
