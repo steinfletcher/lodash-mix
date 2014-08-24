b9c475e960dc6461e8a1bc9405f4602bb049dc06
Stein <steinfletcher@gmail.com>
2014-08-25 00:29:34 +0100

    Correct test name

--- test/isUuid.spec.js
+++ test/isUuid.spec.js
@@ -1,13 +1,13 @@
 var _ = require('../index.js'),
     test = require('tape');
 
-test('uuid: should validate a uuid', function (t) {
+test('isUuid: should validate a uuid', function (t) {
     t.plan(1);
 
     t.equal(_.isUuid(_.uuid()), true);
 });
 
-test('uuid: should reject an invalid uuid', function (t) {
+test('isUuid: should reject an invalid uuid', function (t) {
     t.plan(3);
 
     t.equal(_.isUuid('12345678-1234-1234-1234-123412341234'), false);
